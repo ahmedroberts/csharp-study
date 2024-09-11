a9bcb064a3ced681acff7ea676ca75f1e9f2f48d
Ahmed Roberts <52471253+ahmedroberts@users.noreply.github.com>
2024-09-10 22:21:12 -0700

    Add project files.

---
+++ HelloFromCSharp/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("See you later");
+
+Console.WriteLine("Andre is a anime geek");
+
+Console.WriteLine("Please enter your name: ");
+
+string yourName = Console.ReadLine();
+Console.WriteLine("Goodbye/ " +  yourName);
