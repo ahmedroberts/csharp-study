ac4ec26f8ac3e9aa6c1d2636bf359f92c79e8a7f
Ahmed Roberts <52471253+ahmedroberts@users.noreply.github.com>
2024-09-10 22:30:37 -0700

    :rocket: init CSharp :tada:

--- HelloFromCSharp/Program.cs
+++ HelloFromCSharp/Program.cs
@@ -1,9 +1,50 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("See you later");
+Console.WriteLine("Jumbo Rafiki!\nHello Everybody.\n");
 
-Console.WriteLine("Andre is a anime geek");
+Console.WriteLine("Ahmed is the 9th Raikage.\n");
 
 Console.WriteLine("Please enter your name: ");
 
-string yourName = Console.ReadLine();
-Console.WriteLine("Goodbye/ " +  yourName);
+string? yourName = Console.ReadLine();
+
+Console.WriteLine("Hello " +  yourName + ".");
+
+/* 
+ * Below is my first Console Basics 
+ * from 2023 
+ */
+
+Console.WriteLine("Hello, Friend. I am King Ahmed.");
+
+byte byteNumber = 2;
+int countNumber = 10;
+float totalPrice = 20.94f;
+char character = 'A';
+string firstNameOfKage = "Ahmed";
+bool isWorking = false;
+
+Console.WriteLine(byteNumber);
+Console.WriteLine(countNumber);
+Console.WriteLine(totalPrice);
+Console.WriteLine(character);
+Console.WriteLine(firstNameOfKage);
+Console.WriteLine(isWorking);
+
+Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+
+const float valueOfPi = 3.14f;
+Console.WriteLine("value of Pi: " + valueOfPi);
+
+// FibonacciExample
+int n1 = 0, n2 = 1, n3, i, number;
+Console.Write("Enter the number of elements: ");
+number = int.Parse(Console.ReadLine());
+Console.Write(n1 + " " + n2 + " "); //printing 0 and 1    
+for (i = 2; i < number; ++i) //loop starts from 2 because 0 and 1 are already printed    
+{
+    n3 = n1 + n2;
+    Console.Write(n3 + " ");
+    n1 = n2;
+    n2 = n3; 
+}
