a35a7d70c45b149d7ca718a11e8de0f670f0d805
Ahmed Roberts <52471253+ahmedroberts@users.noreply.github.com>
2024-12-08 04:44:26 -0700

    :wolf: Focused .NET Developer Life, King Raikage Ahmed :octopus:

--- HelloFromCSharp/Program.cs
+++ HelloFromCSharp/Program.cs
@@ -1,13 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("\n-- ================================================================================ -- \n");
+
 Console.WriteLine("Jumbo Rafiki!\nHello Everybody.\n");
 
-Console.WriteLine("Ahmed is the 9th Raikage.\n");
+Console.WriteLine("\nAhmed is the 9th Raikage.\n");
 
-Console.WriteLine("Please enter your name: ");
+Console.WriteLine("\nPlease enter your name: ");
 
 string? yourName = Console.ReadLine();
 
-Console.WriteLine("Hello " +  yourName + ".");
+Console.WriteLine("Hello " +  yourName + ".\n Pleased to make your acquaintance.");
+
+Console.WriteLine("\n-- ================================================================================ -- \n");
+Console.WriteLine("\n-- Console Basics 2023 for Ahmed O. Roberts -- \n");
+Console.WriteLine("\n-- ================================================================================ -- \n");
+
 
 /* 
  * Below is my first Console Basics 
@@ -30,18 +37,28 @@ Console.WriteLine(character);
 Console.WriteLine(firstNameOfKage);
 Console.WriteLine(isWorking);
 
-Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
-Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+Console.WriteLine("\n-- ================================================================================ -- \n");
+//Console.WriteLine("\n-- ================================================================================ -- \n");
+/* 
+ * Console.WriteLine("\n-- ================================================================================ -- \n");
+ * 
+ */
+
+Console.WriteLine("\n-- ================================================================================ -- \n");
+Console.WriteLine("\n-- Fibonaccis Section -- \n");
+
+Console.WriteLine("Fibonacci Section {0} {1}", byte.MinValue, byte.MaxValue);
 
 const float valueOfPi = 3.14f;
-Console.WriteLine("value of Pi: " + valueOfPi);
+Console.WriteLine("\n Value of Pi: " + valueOfPi);
 
 // FibonacciExample
 int n1 = 0, n2 = 1, n3, i, number;
-Console.Write("Enter the number of elements: ");
+Console.Write("\nDo Fibonacci:\nEnter the number of elements: ");
 number = int.Parse(Console.ReadLine());
 Console.Write(n1 + " " + n2 + " "); //printing 0 and 1    
-for (i = 2; i < number; ++i) //loop starts from 2 because 0 and 1 are already printed    
+
+for (  i = 2; i < number; ++i) //loop starts from 2 because 0 and 1 are already printed    
 {
     n3 = n1 + n2;
     Console.Write(n3 + " ");
